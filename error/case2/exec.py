4c3aab121e7bbc7088b985417c7ab7933e84bc2a
numb3r3 <wangfelix87@gmail.com>
2021-06-07 19:32:35 +0800

    fix ...

--- error/case2/exec.py
+++ error/case2/exec.py
@@ -2,7 +2,10 @@ from jina import Executor, requests
 
 
 class MyExecutor(Executor):
+    def __init__(self, bar, **kwargs):
+        super().__init__(**kwargs)
+        self.bar = bar
 
     @requests
-    def foo(self, bar, **kwargs):
-        print(f'bar = {bar}')
+    def foo(self, **kwargs):
+        print(f'bar = {self.bar}')
